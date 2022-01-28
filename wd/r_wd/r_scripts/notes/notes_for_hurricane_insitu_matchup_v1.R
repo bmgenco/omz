ecae9e5b0e68c1fc096745478ccdf1e41dde736c
brandon@europa <bgenco@ucmerced.edu>
2022-01-28 17:41:43 -0500

    weeks worth of work on europa

--- wd/r_wd/r_scripts/notes/notes_for_hurricane_insitu_matchup_v1.R
+++ wd/r_wd/r_scripts/notes/notes_for_hurricane_insitu_matchup_v1.R
@@ -1,3 +1,40 @@
+f.buffer_select<-function(h.pts, function_variables ,cl){
+  
+  # y<-st_combine(st_geometry(cl))
+  # y<-st_centroid(st_geometry(cl))
+  # 
+  # x<-sf:st_buffer(cl, dist=)
+  # x<-st_difference(h.pts ,x)
+  
+  x<-h.pts
+  radi.m<-function_variables$h.radi
+  y<-st_geometry(x)
+  
+  f.circle<-function(y, radi.m){
+    lat0<-y[[1]][2]
+    lon0<-y[[1]][1]
+    center.reproj<-paste0("+proj=aeqd +lat_0=", lat0, " ", "+lon_0=", lon0)
+    flat<-sf::st_transform(y, center.reproj )
+    circle<-sf::st_buffer(flat, dist=radi.m)%>%sf::st_transform(., "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0") 
+    return(circle)
+  }
+  
+  circle<-sf::st_as_sf(f.circle(y, radi.m))
+  names(circle)<-"buffer"
+  st_geometry(circle)<-"buffer"
+  z<-cbind(x, circle)
+  names(z)[names(z) == "geometry"] <- "points"
+  st_geometry(z)<-"buffer"
+  return(z)
+  
+}
+
+
+
+plot(x,  main="Overlap: ODZ-Atlas (red/black) and Hurricane buffer (yellow)",legend=F )
+plot(st_geometry(overlap.odz), col="black", add=T, legend=F)
+plot(filtered_numobs.raster, col="red", add=T, legend=F)
+
 # file not used needs improvement
 # h<-read_csv2(file.path(data_d, "/all_omz_hurricanes/hurdat2-nepac-1949-2020-043021a.txt"),trim_ws=T, col_names = F, show_col_types=F)
 
