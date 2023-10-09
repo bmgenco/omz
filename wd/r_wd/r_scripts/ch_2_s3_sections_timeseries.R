197431294f5b596a70efd381823a092f5ecfbc50
bgenco@ucmerced.edu <bgenco@ucnerced.edu>
2023-10-09 17:00:25 -0400

    work -> home

--- wd/r_wd/r_scripts/ch_2_s3_sections_timeseries.R
+++ wd/r_wd/r_scripts/ch_2_s3_sections_timeseries.R
@@ -23,6 +23,7 @@ source(paste0(my_functions, "ch2_header.R"))
 f.ipak(packages)
 ## get metadata ##
 initialize_argo()
+# load("r_objects/init_argo.RData")
 
 
 #### data ####
@@ -38,6 +39,7 @@ list.100<-data$tc_search_radi_profiles$km_100
 list.200<-data$tc_search_radi_profiles$km_200
 list.500<-data$tc_search_radi_profiles$km_500
 
+rm(data)
 
 
 # olaf
@@ -49,6 +51,12 @@ tc.p<-list.500$tc$EP152021 # can make a function
 
 # specific float
 x<-tc.p%>% filter(. , float == "6903093")
+
+
+x<-tc.p%>% filter(. , float == "5905068")
+
+
+
 s<-f.reformat_profiles_for_ONE_Argo(x)
 
 
@@ -60,38 +68,53 @@ all.floats<-f.reformat_profiles_for_ONE_Argo(tc.p)
 
 
 # trying porfiles
-data.argo<-load_float_data(float_ids =s$float_ids, float_profs = s$float_profs)
+# data.argo<-load_float_data(float_ids =s$float_ids, float_profs = s$float_profs)
 
-x<-data.argo$Data$F6903093
+# x<-data.argo$Data$F6903093
 # y< as.argo(time=x$TIME, longitude = x$LONGITUDE, latitude =  x$LATITUDE, salinity = x$)
 
 #### Plotting ####
 show_sections(float_ids = all.floats$float_ids[3])
 
-show_sections(float_ids = all.floats$float_ids[2], max_depth =150, plot_mld = 2)
+show_sections(float_ids = all.floats$float_ids[1], max_depth =150, plot_mld = 2)
 
 # float 94
 show_sections(float_ids = all.floats$float_ids[1], float_profs = all.floats$float_profs[1], max_depth =150, plot_mld = 2)
 
 
 # show_time_series(float_ids = float.x, plot_depth =50)
-show_time_series(float_ids = s$float_ids, float_profs = s$float_profs, plot_depth =90)
+show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[[1]], plot_depth =90)
 plot.traj<-show_trajectories(float_ids = all.floats$float_ids, float_profs = all.floats$float_profs, return_ggplot = T )
-## add pgplot
-plot.tc_and_trag<-p
+# farting around with ggplot build
+
+
+
+tc.p## add pgplot
+olaf.track<-filter(h.tracks, Key== key_id) %>% st_as_sf(
+ggplot()+geom_sf(data=olaf.track)+plot.traj
 
 # messing with sections
-show_sections(float_ids = s$float_ids, float_profs = s$float_profs[[1]])
+show_sections(float_ids = s$float_ids[1], float_profs = s$float_profs[1])
 
 show_sections(float_ids = s$float_ids[2], float_profs = s$float_profs[2] )
 
-show_trajectories(float_ids = s$float_ids[2], float_profs = s$float_profs[2])
-t.50<-show_time_series(float_ids = s$float_ids[2], float_profs = s$float_profs[2], plot_depth =50)
-t.60<-show_time_series(float_ids = s$float_ids[2], float_profs = s$float_profs[2], plot_depth =60)
-t.70<-show_time_series(float_ids = s$float_ids[2], float_profs = s$float_profs[2], plot_depth =70)
-t.80<-show_time_series(float_ids = s$float_ids[2], float_profs = s$float_profs[2], plot_depth =80)
-t.90<-show_time_series(float_ids = s$float_ids[2], float_profs = s$float_profs[2], plot_depth =90)
-show_profiles(float_ids = s$float_ids[2], float_profs = s$float_profs[2])
+show_trajectories(float_ids = s$float_ids[1], float_profs = s$float_profs[1])
+t.50<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =50)
+dev.copy2pdf(file="t1_50.pdf")
+t.60<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =60)
+t.70<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =70)
+t.80<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =80)
+t.90<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =90)
+t.100<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =100)
+show_profiles(float_ids = s$float_ids[1], float_profs = s$float_profs[1])
+
+
+t.50<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =50, variables=c("DOXY", "TEMP"))
+t.60<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =60, variables=c("DOXY", "TEMP", "CHLA"))
+t.70<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =70, variables=c("DOXY", "TEMP", "CHLA"))
+t.80<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =80, variables=c("DOXY", "TEMP")
+t.90<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =90, variables=c("DOXY", "TEMP", "CHLA"))
+t.100<-show_time_series(float_ids = s$float_ids[1], float_profs = s$float_profs[1], plot_depth =100, variables=c("DOXY", "TEMP", "CHLA"))                                             
 
 ### oce object
 
